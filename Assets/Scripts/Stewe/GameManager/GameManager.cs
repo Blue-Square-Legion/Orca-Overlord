ad2f010d01e471f140aefcda2d677a18a80c3efa
vvayachal <41337015+vvayachal@users.noreply.github.com>
2024-11-05 15:11:31 -0500

    Added Countdown Timer.
    Created TimerContainer Object to enable Designers to add multiple timers with different duration (possibly not needed and can be removed).

--- Assets/Scripts/Stewe/GameManager/GameManager.cs
+++ Assets/Scripts/Stewe/GameManager/GameManager.cs
@@ -1,15 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance;
+    private static GameManager _instance;
+    [SerializeField] private CountdownTimerContainer[] countdownTimers;
+
+    [Header("UI Components")] 
+    [SerializeField]public TextMeshProUGUI timerText;
+    
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("Game Manager Instance is Null!");
+            }
+            
+            return _instance;
+        }
+    }
 
     private void Awake()
     {
-        instance = this;
+        _instance = this;
+
+        if (countdownTimers.Length > 0)
+        {
+            foreach (CountdownTimerContainer timer in countdownTimers)
+            {
+                timer.countdownTimer = gameObject.AddComponent<CountdownTimer>();
+                timer.countdownTimer.SetTimerDuration(timer.timerDuration);
+                timer.countdownTimer.SetTimerText(timerText);
+            }
+        }
     }
 
-
+    private void Update()
+    {
+        if (!countdownTimers[0].countdownTimer.IsCountingDown)
+        {
+            countdownTimers[0].countdownTimer.StartTimer();
+        }
+    }
 }

15e16fdf4268f698437207b024853f7d1029efa4
vvayachal <41337015+vvayachal@users.noreply.github.com>
2024-11-21 17:02:23 -0500

    Player Camera Fix
    Fixed camera movement due to mesh and collider misalignment.

--- Assets/Scripts/NPCs/Dolphin.cs
+++ Assets/Scripts/NPCs/Dolphin.cs
@@ -3,21 +3,30 @@ using UnityEngine;
 
 public class Dolphin : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float distanceThreshold;
-    
+
+    private GameObject _player;
+    private void Awake()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        if (!_player)
+        {
+            Debug.LogError("Player Not Found!");
+        }
+    }
+
     private void Update()
     {
-        transform.LookAt(player.transform);
+        transform.LookAt(_player.transform);
         
-        float distance = Vector3.Distance(transform.position, player.transform.position);
+        float distance = Vector3.Distance(transform.position, _player.transform.position);
 
         // Check if the object should move towards the player
         if (distance > distanceThreshold)
         {
             // Move towards the player's position
-            Vector3 direction = (player.transform.position - transform.position).normalized;
+            Vector3 direction = (_player.transform.position - transform.position).normalized;
             transform.position += direction * moveSpeed * Time.deltaTime;
         }
     }

fa8157b87b6c19a54317639ab88f395a693d2d06
RLamo3 <ronaldlamo3@gmail.com>
2024-11-27 22:00:49 -0500

    Cant figure out the boat shake
    I'm trying to reference all the children triggers with this.transform.Find so i can attach this script to the parent object because if i have the script attached to one trigger it doesnt update the others  when its ready to flip. I was able to get the boats to shake a variable amount of times depending on the boat but not able to flip because the script was attached to the trigger and not the parent. Also while I'm writing this out, all 3 of my animators just stopped working, i cant see or edit any of them.

--- Assets/Scripts/Ronald/BoatFlipAnimator.cs
+++ Assets/Scripts/Ronald/BoatFlipAnimator.cs
@@ -4,26 +4,80 @@ using UnityEngine;
 
 public class BoatFlipAnimator : MonoBehaviour
 {
+    public int maxPlays;
 
     private Animator animator;
-
+    private int playCount = 0;
+    public bool flipReady = false;
+    private bool shakeReload = false;
 
     // Start is called before the first frame update
     void Awake()
     {
-        animator = transform.parent.GetComponent<Animator>();
+        animator = transform.GetComponent<Animator>();
+
+        //this should get the children triggers of the boats that the script is attached to
+        Transform ShakeTriggerTransform = this.transform.Find("ShakeTrigger");
+        Transform BoatFlipRightTriggerTransform = this.transform.Find("BoatFlipTriggerRight");
+        Transform BoatFlipLeftTriggerTransform = this.transform.Find("BoatFlipTriggerLeft");
+
+        GameObject ShakeTrigger = ShakeTriggerTransform.gameObject;
+        GameObject BoatFlipTriggerRight = BoatFlipRightTriggerTransform.gameObject;
+        GameObject BoatFlipTriggerLeft = BoatFlipLeftTriggerTransform.gameObject;
     }
 
-    private void OnTriggerEnter(Collider other)
+    private IEnumerator OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && gameObject.name == "BoatFlipTriggerRight")
+        //shake the boat if the player runs into the shake trigger
+        //trying to make this for loop interchangeable with each boat and then set flipReady to true 
+        //so the next time the player enters a trigger it will flip the baot
+        if (other.CompareTag("Player") && gameObject.name == "ShakeTrigger" && flipReady == false)
+        {
+            Debug.Log("hit the trigger");
+            for (int i = playCount; i < maxPlays; i++)
+            {
+                animator.SetTrigger("Shake");
+                float animDuration = animator.GetCurrentAnimatorStateInfo(0).length;
+                yield return new WaitForSeconds(animDuration);
+
+            }
+            flipReady = true;
+        }
+
+        //flip boat to either side after shaking the boat
+        if (other.CompareTag("Player") && gameObject.name == "BoatFlipTriggerRight" && flipReady == true)
         {
             animator.SetTrigger("flipRight");
         }
-        
-        if (other.CompareTag("Player") && gameObject.name == "BoatFlipTriggerLeft")
+
+        if (other.CompareTag("Player") && gameObject.name == "BoatFlipTriggerLeft" && flipReady == true)
         {
             animator.SetTrigger("flipLeft");
+        }
+    }
+
+    IEnumerator PlayAnimationLoop()
+    {
+        // Play the animation for a fixed number of times (e.g., 5 times)
+        for (int i = playCount; i < maxPlays; i++)
+        {
+            if (shakeReload)  // Ensure we're still inside the trigger zone
+            {
+                animator.SetTrigger("Shake");  // Trigger the animation
+                playCount++;  // Increment the play count
+
+                // Wait for the animation to finish (this is optional based on your timing)
+                float animationLength = animator.GetCurrentAnimatorStateInfo(0).length;
+                yield return new WaitForSeconds(animationLength);  // Wait for the animation length
+
+                Debug.Log("Animation played " + playCount + " times.");
+            }
+            else
+            {
+                // If player leaves trigger before 5 times, stop playing
+                Debug.Log("Player left the trigger zone.");
+                break;
+            }
         }
     }
 }

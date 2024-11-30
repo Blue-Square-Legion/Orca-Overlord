7a73dbfacbdb86cbe27449eaca019e1d84b1c3cc
RLamo3 <ronaldlamo3@gmail.com>
2024-11-29 22:43:41 -0500

    Issue resolved
    I was trying to define the Triggers in the if statement, but since I had already done that in the awake function, all I had to do was call the GameObject that was defined in the if statement without all the other.gameobject search functions within the if statement. Animations play properly but not entirely as intended

--- Assets/Scripts/Ronald/BoatFlipAnimator.cs
+++ Assets/Scripts/Ronald/BoatFlipAnimator.cs
@@ -9,75 +9,72 @@ public class BoatFlipAnimator : MonoBehaviour
     private Animator animator;
     private int playCount = 0;
     public bool flipReady = false;
-    private bool shakeReload = false;
+
+    private GameObject shakeTrigger;
+    private GameObject boatFlipRightTrigger;
+    private GameObject boatFlipLeftTrigger;
 
     // Start is called before the first frame update
     void Awake()
     {
         animator = transform.GetComponent<Animator>();
+        // Find child triggers by name
+        shakeTrigger = transform.Find("ShakeTrigger")?.gameObject;
+        boatFlipRightTrigger = transform.Find("BoatFlipTriggerRight")?.gameObject;
+        boatFlipLeftTrigger = transform.Find("BoatFlipTriggerLeft")?.gameObject;
 
-        //this should get the children triggers of the boats that the script is attached to
-        Transform ShakeTriggerTransform = this.transform.Find("ShakeTrigger");
-        Transform BoatFlipRightTriggerTransform = this.transform.Find("BoatFlipTriggerRight");
-        Transform BoatFlipLeftTriggerTransform = this.transform.Find("BoatFlipTriggerLeft");
-
-        GameObject ShakeTrigger = ShakeTriggerTransform.gameObject;
-        GameObject BoatFlipTriggerRight = BoatFlipRightTriggerTransform.gameObject;
-        GameObject BoatFlipTriggerLeft = BoatFlipLeftTriggerTransform.gameObject;
+        // Log to ensure the triggers are correctly found
+        if (shakeTrigger != null) Debug.Log("Found ShakeTrigger.");
+        if (boatFlipRightTrigger != null) Debug.Log("Found BoatFlipTriggerRight.");
+        if (boatFlipLeftTrigger != null) Debug.Log("Found BoatFlipTriggerLeft.");
     }
 
-    private IEnumerator OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        //shake the boat if the player runs into the shake trigger
-        //trying to make this for loop interchangeable with each boat and then set flipReady to true 
-        //so the next time the player enters a trigger it will flip the baot
-        if (other.CompareTag("Player") && gameObject.name == "ShakeTrigger" && flipReady == false)
-        {
-            Debug.Log("hit the trigger");
-            for (int i = playCount; i < maxPlays; i++)
-            {
-                animator.SetTrigger("Shake");
-                float animDuration = animator.GetCurrentAnimatorStateInfo(0).length;
-                yield return new WaitForSeconds(animDuration);
+        
+        if (!other.CompareTag("Player")) return;
+        //Debug.Log($"Triggered by: {other.gameObject.name} (Tag: {other.tag})");
 
-            }
-            flipReady = true;
+        // Check which trigger this script is attached to
+        if (other.gameObject.name == ("Player") && shakeTrigger && !flipReady)
+        {
+
+            //Debug.Log("ShakeTrigger activated by Player."); 
+            StartCoroutine(ShakeBoat());
         }
 
-        //flip boat to either side after shaking the boat
-        if (other.CompareTag("Player") && gameObject.name == "BoatFlipTriggerRight" && flipReady == true)
+        //flip boat to right
+        else if (other.gameObject.name == ("Player") && boatFlipRightTrigger && flipReady)
         {
+            Debug.Log("Flip Right");
             animator.SetTrigger("flipRight");
         }
 
-        if (other.CompareTag("Player") && gameObject.name == "BoatFlipTriggerLeft" && flipReady == true)
+        //flip boat to left
+        else if (other.gameObject.name == ("Player") && boatFlipLeftTrigger && flipReady)
         {
+            Debug.Log("Flip Left");
             animator.SetTrigger("flipLeft");
+        }
+        else
+        {
+            Debug.Log("No valid trigger hit.");
         }
     }
 
-    IEnumerator PlayAnimationLoop()
+    //shake the boat if the player runs into the shake trigger
+    private IEnumerator ShakeBoat()
     {
-        // Play the animation for a fixed number of times (e.g., 5 times)
+       
         for (int i = playCount; i < maxPlays; i++)
         {
-            if (shakeReload)  // Ensure we're still inside the trigger zone
-            {
-                animator.SetTrigger("Shake");  // Trigger the animation
-                playCount++;  // Increment the play count
-
-                // Wait for the animation to finish (this is optional based on your timing)
-                float animationLength = animator.GetCurrentAnimatorStateInfo(0).length;
-                yield return new WaitForSeconds(animationLength);  // Wait for the animation length
-
-                Debug.Log("Animation played " + playCount + " times.");
-            }
-            else
-            {
-                // If player leaves trigger before 5 times, stop playing
-                Debug.Log("Player left the trigger zone.");
-                break;
-            }
+            animator.SetTrigger("Shake");
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            float animationLength = stateInfo.length;
+            yield return new WaitForSeconds(animationLength);
+            Debug.Log("Played " + i + " times");
         }
+        flipReady = true;
+
     }
 }
